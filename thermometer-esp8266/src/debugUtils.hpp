c9b864b54204bc315353f42d584a7637415b4c46
Bezalel Cohen <bezalelcohen1@gmail.com>
2023-08-18 17:43:15 +0300

    Bubfix: add empty define for DEBUG_MODE_SERIAL_BEGIN when debug_mode = false

--- thermometer-esp8266/src/debugUtils.hpp
+++ thermometer-esp8266/src/debugUtils.hpp
@@ -1,13 +1,12 @@
 #if DEBUG_MODE
 // serial monitor
 #define MONITOR_SPEED 115200U
-
 #define DEBUG_MODE_SERIAL_BEGIN Serial.begin(MONITOR_SPEED)
+#undef MONITOR_SPEED
 #define DEBUG_MODE_SERIAL_PRINT(str) Serial.print(str)
 #define DEBUG_MODE_SERIAL_PRINTLN(str) Serial.println(str)
 #else
+#define DEBUG_MODE_SERIAL_BEGIN
 #define DEBUG_MODE_SERIAL_PRINT(str)
 #define DEBUG_MODE_SERIAL_PRINTLN(str)
-
-// #undef MONITOR_SPEED
 #endif
